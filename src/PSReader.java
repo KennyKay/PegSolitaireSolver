7497944aa2ca788824c5289d9f3df2b92cdddfaf
Hugo Lapointe Di Giacomo <hugo.lapo@gmail.com>
2017-02-04 22:00:58 -0500

    Rought initial commit. To be continue.

---
+++ src/PSReader.java
@@ -0,0 +1,20 @@
+import java.util.Collection;
+
+/**
+ *
+ */
+public class PSReader {
+
+	public PSBoard create(final String path) {
+
+		//TODO
+		return null;
+	}
+
+
+	private Collection<PSBoard.PSMove> generateMoves() {
+
+		//TODO
+		return null;
+	}
+}
---
+++ src/PSSolver.java
@@ -0,0 +1,13 @@
+import java.util.Collection;
+
+/**
+ *
+ */
+public class PSSolver {
+
+	public final Collection<PSBoard> solve(final PSBoard board) {
+
+		//TODO
+		return null;
+	}
+}
